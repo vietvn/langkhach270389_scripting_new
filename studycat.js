812f2f18f13f27599080b9d9bb99d53e2c5d87c9
vietvn <59811120+vietvn@users.noreply.github.com>
2024-03-29 08:54:38 +0700

    Update studycat.js

--- studycat.js
+++ studycat.js
@@ -1,3 +1,7 @@
+var obj = JSON.parse($response.body);
+
+obj.data = 
+{
 "currency":"USD",
 "subscription_id":"2000000510526219",
 "purchased":"2024-03-28T13:28:18.000Z",
@@ -16,3 +20,7 @@
 "purchased_ms":1711632498000,
 "type":"renewal",
 "Items":[{"latest_expired_receipt_info":{"original_transaction_id":"2000000510526219","transaction_id":"2000000557944974","product_id":"com.studycat.sa.fe.t7d_12m.a60m15_202305_1","expires_date_formatted":"2024-03-28T14:28:18.000Z","expires_ms":1711636098000,"purchaseToken":"2000000510526219","expirationTime":1711636098000}}]
+    };
+$done({body: JSON.stringify(obj)});
+
+// Descriptionsp
