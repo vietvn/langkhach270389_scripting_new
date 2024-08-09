9b0dea9ae86bdb9b13c6d5be4754c6e7511c90a1
vietvn <59811120+vietvn@users.noreply.github.com>
2024-08-10 04:46:48 +0700

    Update Adguard.js

--- Adguard.js
+++ Adguard.js
@@ -1,3 +1,14 @@
 let obj = JSON.parse($response.body);
-obj={"products":[{"product_id":"com.adguard.lifetimePurchase","premium_status":"ACTIVE"}]};
+obj={
+  "canceled" : null,
+  "cancel_at" : null,
+  "description" : null,
+  "show_upgrade" : false,
+  "is_apple_subscription" : false,
+  "next_billing_amount" : null,
+  "show_payment_method" : false,
+  "plan_name" : "Premium",
+  "currency" : null,
+  "next_billing_date" : null
+};
 $done({body: JSON.stringify(obj)});
