2ed779710291063668fd3c60213a4493b46f48e4
vietvn <59811120+vietvn@users.noreply.github.com>
2024-08-09 22:24:30 +0700

    Update CamScaner.js

--- CamScaner.js
+++ CamScaner.js
@@ -1,9 +1,9 @@
 let obj = JSON.parse($response.body);
 obj = {"data":{
   "is_verified" : true,
-  "has_vip_access" : false,
+  "has_vip_access" : true,
   "has_newsletter" : false,
-  "has_active_subscription" : false,
+  "has_active_subscription" : true,
   "is_oauth" : true,
   "logged_in" : true,
   "image" : "https://fra1.digitaloceanspaces.com/pharmuni/pharmuni-main/profile_image/user-b41b5b4a-5cec-44f7-babf-6a9fd6863073-user-image-6af88341-cd9f-4819-aa9a-10da7b3cce2f.png?AWSAccessKeyId=24FZNUKRY5LCMGI5ULAO&Signature=AyKazb2dmdCl0kbILZJQHKvd%2FbM%3D&Expires=1723216389",
